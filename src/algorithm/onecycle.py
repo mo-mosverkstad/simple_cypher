1869b06abe57e4e8f3cf1c2b90c305426078949a
mo-mosverkstad <mo.mosverkstad@gmail.com>
2017-12-24 14:41:21 +0100

    Support onecycle decryption and railfence encryption.

--- src/algorithm/onecycle.py
+++ src/algorithm/onecycle.py
@@ -12,7 +12,15 @@ class onecycle(base_transposition):
         newl = l[1:] + l[:1]
         d = dict(zip(l,newl))
         return self.transposition_core(d,text)
+    
+    def __dec_onecycle(self,text):
+        l = []
+        for i in range(self.key-1,len(text),self.key):
+            l.append(i)
+        newl = l[1:] + l[:1]
+        d = dict(zip(newl,l))
+        return self.transposition_core(d,text)
 
     def cypher(self, text):
         if self.howto == 'enc': return self.__enc_onecycle(text)
-        #else: return self.__dec_onecycle(text)
+        else: return self.__dec_onecycle(text)
---
+++ src/algorithm/railfence.py
@@ -0,0 +1,30 @@
+import operator
+from .base_transposition import base_transposition
+
+class railfence(base_transposition):
+    def __init__(self, howto, key):
+        super().__init__(howto, key)
+        self.key = int(key)
+
+    def __generate_railfence_list(self, text_len):
+        l = (list(range(self.key))+list(range(self.key-2,0,-1)))
+        repeat = int(text_len/(2*self.key-2))
+        rem = text_len%(2*self.key-2)
+        long_list = l*repeat + l[:rem]
+        railfence_dict = dict(zip(list(range(text_len)), long_list))
+        sorteds = sorted(railfence_dict.items(), key=operator.itemgetter(1))
+        return [i[0] for i in sorteds]
+
+    def __generate_railfence_dict(self, orig_index_list, railfence_list):
+        return dict(zip(railfence_list, orig_index_list))
+
+    def __enc_railfence(self,text):
+        return self.transposition_core( \
+            self.__generate_railfence_dict(list(range(len(text))), self.__generate_railfence_list(len(text))), \
+            text)
+        
+
+    def cypher(self, text):
+        if self.howto == 'enc': return self.__enc_railfence(text)
+        #else: return self.__dec_onecycle(text)
+        

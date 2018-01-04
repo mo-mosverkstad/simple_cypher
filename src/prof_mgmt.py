0341ed93ecb494f96c47cc98f6fcd4aa1041b06f
mo-mosverkstad <mo.mosverkstad@gmail.com>
2018-01-04 19:25:19 +0100

    Interface functions of prof_mgmt

---
+++ src/prof_mgmt.py
@@ -0,0 +1,31 @@
+# ========================= CODING FILE INFORMATION ========================= #
+# File         : prof_mgmt.py
+# Date         : 2018-Jan-04
+# Last modified: 2018-Jan-04
+#
+# Author       : Mo
+#
+# Description  : This file is used for profile management
+#
+# Dependency   : Python 3.6
+#
+# Usage        : -
+# =========================================================================== #
+
+# prof_list () -> dictionary of all of profiles, {mapping: [caesar, atbash,...], 
+#                                                 transposition: [...]}
+# prof_new  (type, algorithm) -> None
+# prof_edit (algorithm, code) -> None
+# prof_delete (algorithm) -> None
+
+def prof_list ():
+    pass
+
+def prof_new (type, algorithm):
+    pass
+
+def prof_edit (algorithm, code):
+    pass
+
+def prof_delete (algorithm):
+    pass
